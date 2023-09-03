1b67f280398d1bf290381df7b1ad1afe30a91424
Sediqwe <sediqwe@gmail.com>
2023-09-03 12:00:31 +0200

    Kereső rendezés

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -4,7 +4,7 @@ class LinesController < ApplicationController
   # GET /lines
   def index
     @q = Line.ransack(params[:q])
-    @lines = @q.result(distinct: true).page(params[:page])
+    @lines = @q.result().order(contentuid: :DESC).page(params[:page])
   end
 
   # GET /lines/1
