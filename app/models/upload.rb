355ba5217bd987cdea727ca45da033e7709af0ec
Sediqwe <sediqwe@gmail.com>
2023-09-08 11:27:00 +0200

    destroy_all -> delete_all

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,6 +2,6 @@ class Upload < ApplicationRecord
   belongs_to :game
   belongs_to :user
   has_one_attached :file, dependent: :destroy
-  has_many :lines, dependent: :destroy_all
+  has_many :lines, dependent: :delete_all
   
 end
